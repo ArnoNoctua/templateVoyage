fb6cd3faffbe338f63d36a08e5255aea86a2678c
Arnaud <1467476@csfoy.ca>
2022-05-26 15:20:35 -0400

    build

---
+++ build/precache-manifest.47d52274d33ba16cd5c90a4cfb71d003.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abafb7757caa1774b33c6eb7600530e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b65fb5b17ff93d686845",
+    "url": "/static/css/main.730cc97c.chunk.css"
+  },
+  {
+    "revision": "ad3011d31ddfa19666e4",
+    "url": "/static/js/2.a2dde473.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.a2dde473.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b65fb5b17ff93d686845",
+    "url": "/static/js/main.37b920bf.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
